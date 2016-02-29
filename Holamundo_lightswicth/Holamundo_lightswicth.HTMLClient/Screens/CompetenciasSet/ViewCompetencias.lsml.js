f555546f8607c3d2df438992054d27a867228e8b
elkinpertuz6 <elccoco1995@hotmail.com>
2016-02-29 11:31:52 -0500

    competencias

---
+++ Holamundo_lightswicth/Holamundo_lightswicth.HTMLClient/Screens/CompetenciasSet/ViewCompetencias.lsml.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.ViewCompetencias.Details_postRender = function (element, contentItem) {
+    // Write code here.
+    var name = contentItem.screen.competenciasItem.details.getModel()[':@SummaryProperty'].property.name;
+    contentItem.dataBind("screen.competenciasItem." + name, function (value) {
+        contentItem.screen.details.displayName = value;
+    });
+}
+
